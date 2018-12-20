a4768d90bc1622c79b00d84d74ee7907ebae0084
RajendraKSahoo <45260260+RajendraKSahoo@users.noreply.github.com>
2018-12-20 17:50:04 +0530

    Add files via upload

--- UserPage.java
+++ UserPage.java
@@ -6,15 +6,27 @@ import java.util.HashMap;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
 
 import com.ibm.utilities.PropertiesFileHandler;
 
 public class UserPage 
 {
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	public UserPage(WebDriver driver,WebDriverWait wait)
+	{
+		this.driver=driver;
+		this.wait=wait;
+		PageFactory.initElements(driver,this);
+	}
 
 //	@Test
 	public void verifytab() throws IOException, InterruptedException
@@ -48,5 +60,63 @@ public class UserPage
 		
 				
 	}
+	
+	//WebElement for link SignUp
+		@FindBy(xpath="//a[contains(text(),'SignUp')]")
+		WebElement signupEle;
+	//WebElement for field Full Name
+		@FindBy(xpath="//input[@id='name']")
+		WebElement nameEle;
+	//WebElement for field Phone Number
+		@FindBy(xpath="//input[@id='pnum']")
+		WebElement pnumEle;
+	//WebElement for field Password
+		@FindBy(xpath="//input[@id='password']")
+		WebElement pwdEle;
+	//WebElement for field Confirm Password
+		@FindBy(xpath="//input[@id='cpassword']")
+		WebElement cpwdEle;
+	//WebElement for checkbox Term & Conditions
+		@FindBy(xpath="//input[@id='tccheckbox']")
+		WebElement tccEle;
+	//WebElement for button SignUp
+		@FindBy(xpath="//button[@id='mem_signup']")
+		WebElement bsignupEle;
+		
+		public void clickOnLinkSignUp()
+		{
+			signupEle.click();
+		}
+		public void enterFullName(String fullname)
+		{
+			nameEle.sendKeys(fullname);
+		}
+		public void enterPhoneNo(String pno)
+		{
+			pnumEle.sendKeys(pno);
+		}
+		public void enterPassword(String newpassword)
+		{
+			pwdEle.sendKeys(newpassword);
+		}
+		public void enterConfirmPassword(String confpassword)
+		{
+			cpwdEle.sendKeys(confpassword);
+		}
+		public void clickOnTermCondition()
+		{
+			tccEle.click();
+		}
+		public void clickOnButtonSignUp()
+		{
+			bsignupEle.click();
+		}
+		public void clickOnAlertBox()
+		{
+			Alert alertBox = driver.switchTo().alert();
+			String alertmsg = alertBox.getText();
+			System.out.println(alertmsg);
+			alertBox.accept();
+		}
 
 }
